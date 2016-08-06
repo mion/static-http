f0a037b747caa00bc46514a6d8c2ca37037fe1ca
Gabriel Vieira <gluisvieira@gmail.com>
2016-08-06 15:38:21 -0300

    Show red help instructions on error

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 #!/usr/bin/env node
-var express = require('express');
-var app = express();
+var program = require("commander")
+  , colors = require("colors")
+  , express = require("express");
 
-var path = process.argv[2];
-var port = process.argv[3];
+program
+  .version("1.0.0")
+  .description("Serve static files through HTTP")
+  .usage("[options] <dir>")
+  .option("-p, --port <port>", "Specify on which port to run the HTTP server", /^\d+$/i, "3600")
+  .parse(process.argv);
 
-app.use(express.static(path));
+if (program.args.length === 0) {
+  console.log("[ERROR] You need to specify a directory.");
+  var makeRed = function (txt) { return colors.red(txt); };
+  program.outputHelp(makeRed);
+} else {
+  var app = express()
+    , dir = program.args[0]
+    , port = parseInt(program.port);
 
-app.listen(1629, function () {
-  console.log('[*] Static server running on port 1629, serving files from: ' + path);
-});
+  app.use(express.static(dir));
+
+  app.listen(port, function () {
+    console.log("[*] Static server running on port "+port+", serving files from: " + dir);
+  });
+}
