08fa6ce1005e31cc6de8968491a6eca1f5ce262d
Gabriel Vieira <gluisvieira@gmail.com>
2021-08-24 14:56:32 -0300

    Improve logging DX

--- server.js
+++ server.js
@@ -23,7 +23,13 @@ if (program.args.length === 0) {
 
   app.use(express.static(dir));
   app.listen(port, function () {
-    console.log(colors.green("[*] Static server running on port "+port+", serving files from: " + dir));
+    console.log(colors.green('\n\t[*]'), colors.bgGreen.black(' Static HTTP server running... '))
+    console.log(colors.green('\t * '))
+    console.log(colors.green('\t * '), colors.green('Port:'), colors.white(port.toString()))
+    console.log(colors.green('\t * '), colors.green('Folder:'), colors.white(dir.toString()))
+    console.log(colors.gray('\n\tWARNING\n\tAny files in that folder can be accessed if your IP is public!'))
+    console.log('\n\tVisit', colors.cyan(`http://localhost:${port}`), 'URL to see your site.\n')
+    console.log('\tPress', colors.yellow('Control + C'), 'to stop the server.\n')
   }).on("error", function (err) {
     if (err.errno === "EADDRINUSE") {
       console.error(colors.red("[ERROR] The specified port " + port + " is already in use."));
